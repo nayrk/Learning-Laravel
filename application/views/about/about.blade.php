1b887fa2f85e22c9bd3e9076769ec93be0e61ffa
Anthony Doan <mythicalprogrammer@gmail.com>
2012-12-06 15:47:18 -0800

    add profiles to About page

--- application/views/about/about.blade.php
+++ application/views/about/about.blade.php
@@ -1,1 +1,34 @@
-<?php echo "hi"; ?>
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Unrequited Love</h1>
+                <p>Our Team..</p>
+            </div>
+
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="span4">
+                    <h3>Anthony Doan,</h3>
+                    <h4>CEO/CTO</h4>
+                    <p>Founded Unrequited Love.</p>
+                    <p>Programmer, hacker, system administrator...</p>
+                    <p><a class="btn" href="#">View resume&raquo;</a></p>
+                </div>
+                <div class="span4">
+                    <h3>Ryan Kwong,</h3>
+                    <h4>Programmer</h4>
+                    <p>Programmer.</p>
+                    <p><a class="btn" href="#">View resume&raquo;</a></p>
+               </div>
+                <div class="span4">
+                    <h3>Hubert Shen,</h3>
+                    <h4>Programmer, Artist, Designer</h4>
+                    <p>Programmer.</p>
+                    <p><a class="btn" href="#">View resume&raquo;</a></p>
+                </div>
+            </div>
+
+            <hr>
+
+	@include('partials.footer')
+
+        </div> <!-- /container -->
